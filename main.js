cdd85364bccabfb17bfb9992eadd8d5fcc1e7e18
Alexandru Bej <alexbej07@gmail.com>
2023-09-28 15:43:11 +0200

    Refactor code

--- main.js
+++ main.js
@@ -1,28 +1,27 @@
 $(document).ready(function () {
+    // Set placement position for popover
     var placementPosition = "left";
 
     if (window.screen.width < 769) {
         placementPosition = "bottom";
     }
 
+    // Open popover
     $('[data-bs-toggle="popover"]').popover({
         placement: placementPosition,
         html: true,
-        content: '<div class="popover-container-top"><img src="images/jpg/Profile_img_thumbnail.png" width="100" alt="Alexandru Bej"><div class="popover-info"><h3>Alexandru Bej</h3><h5>Web Developer | Graphic Designer</h5><div class="contact-methods"><a href="https://www.linkedin.com/in/alexandru-bej-203a72ab/"target = "_blank"><i class="fa-brands fa-linkedin contact-icon"></i></a ><a href="mailto:alexbej07@gmail.com"><i class="fa-solid fa-envelope contact-icon"></i></a><div><i class="fa-solid fa-phone contact-icon"></i><span id="data">+39 637 951 478</span></div></div ></div ></div ><div class="popover-container-bottom"><a id="download" target="_blank" href="assets/CV-Bej_Alexandru.pdf">Download PDF version</a></div>'
+        content: '<div class="popover-container-top"><img src="images/jpg/Profile_img_thumbnail.png" width="100" alt="Alexandru Bej"><div class="popover-info"><h3>Alexandru Bej</h3><h5>Web Developer | Graphic Designer</h5><div class="contact-methods"><a href="https://www.linkedin.com/in/alexandru-bej-203a72ab/"target = "_blank"><i class="fa-brands fa-linkedin contact-icon"></i></a ><a href="mailto:alexbej07@gmail.com"><i class="fa-solid fa-envelope contact-icon"></i></a><div><i class="fa-solid fa-phone contact-icon"></i><span id="data">+39 637 951 478</span></div></div ></div ></div ><div class="popover-container-bottom"><a id="download" target="_blank" href="assets/CV-Bej_Alexandru.pdf" download type="application/pdf">Download PDF version</a></div>'
     });
 
-
+    // Activate back to top button
     $('#back-to-top-btn').on('click', function (e) {
         $("html, body").animate({ scrollTop: $("#top").offset().top }, 500);
     });
 
-    // Init variable for timer
+    // Variables for open popover after scrolling over an element
     let timer;
-    // Get target element
     const elementToScrollBy = $("#about");
     const elementToHide = $("#hidden-container");
-    // Get viewport height
-    const screen = window.innerHeight;
 
     // Fire callback on window scroll
     $(window).scroll(function () {
@@ -31,25 +30,22 @@ $(document).ready(function () {
         timer = setTimeout(check_el_pos, 100);
     });
 
+    // Check element position and add or remove class for displaying
     function check_el_pos() {
-        // Clear the timer
         clearTimeout(timer);
-        // Get current scroll position
         let scroll_pos = $(window).scrollTop();
-        // This is the math here. Add scroll position to screen height and you get the bottom of the screen. When that value equals the top offset of the element, we are there. 
         if (scroll_pos > elementToScrollBy.offset().top) {
-            // Add the classes to the element. Boom, we're done.
             elementToHide.removeClass("displayed").addClass("displayed");
         } else {
             elementToHide.removeClass("displayed")
         }
     }
 
+    // Close popover with outside click
     $('body').on('click', function (e) {
-        //did not click a popover toggle, or icon in popover toggle, or popover
         if ($(e.target).data('toggle') !== 'popover'
             && $(e.target).parents('[data-bs-toggle="popover"]').length === 0
-            && $(e.target).parents('.popover.in').length === 0) { 
+            && $(e.target).parents('.popover.in').length === 0) {
             $('[data-bs-toggle="popover"]').popover('hide');
         }
     });
